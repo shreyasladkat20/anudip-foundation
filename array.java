f89337313074505cc8baa8c404130ccb9f7e4a1a
shreyasladkat20 <shreyasladkat208@gmail.com>
2025-06-10 21:46:40 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,10 @@
+public class array {
+    public static void main(String[] args) {
+        //int[] arr=new int[4];
+        int[] arr={1,2,3,4,5};
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+    
+}
